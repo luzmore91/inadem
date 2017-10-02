f704b70ee4c5bdbffd7fd99a680cbae5dfa325ee
Mercedes Miranda <mervic1706@gmail.com>
2017-10-02 12:53:40 -0500

    Cambios en modificar meche

--- resources/views/modificar.blade.php
+++ resources/views/modificar.blade.php
@@ -2,43 +2,53 @@
 <br>
 <h4>*<i>Haga doble click encima de un campo para editarlo.</i></h4>
             <table id="datos" class="table table-hover table-condensed" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Título comercial</th>
-                        <th>Problemática a resolver</th>
-                        <th>Descripción/Resumen</th>
-                        <th>Institución</th>
-                        <th>Tipo de invención</th>
-                    </tr>
-                </thead>
+                
                 <tbody>
+                  <tr>
+                     <div class="form-group col-md-12">
+                        <label for="titulo" ><strong>Título:</strong></label>
+                         <textarea style="resize: vertical" class="form-control col-md-6" id="tituloProy" rows="2">{{ $proyecto[0]->titulo }}</textarea>
+                     </div>
+                     <div class="form-group col-md-12">
+                         <label for="tituloComercial"><Strong>Título comercial:</Strong></label>
+                         <textarea style="resize: vertical" class="form-control" id="tituloComercial" rows="2">{{ $proyecto[0]->tituloComercial}}</textarea>
+                     </div>
+                 </tr>
+                 <tr>
+                     <div class="form-group col-md-12">
+                            <label for="problematica"><strong>Problemática a resolver:</strong></label>
+                            <textarea style="resize: vertical" class="form-control" id="problematica" rows="6">{{ $proyecto[0]->problematica}}</textarea>
+                        </div>
+                
+                        <div class="form-group col-md-12">
+                            <label for="descripcion"><strong>Descripción / resumen:</strong></label>
+                            <textarea style="resize: vertical" class="form-control" id="descripcion" rows="6">{{ $proyecto[0]->descripcion}}</textarea>
+                        </div>
+                        </tr>
 
-                    <tr>
-                        <td><textarea class="form-control" id="tituloProy">{{ $proyecto[0]->titulo }}</textarea></td>
-                        <td><textarea class="form-control" id="tituloComercial">{{ $proyecto[0]->tituloComercial}}</textarea></td>
-                        <td><textarea class="form-control" id="problematica">{{ $proyecto[0]->problematica}}</textarea></td>
-                        <td><textarea class="form-control" id="descripcion">{{ $proyecto[0]->descripcion}}</textarea></td>
-                        <td>
-                            <select id="institucion" required class="form-control selectpicker" data-style="btn-green" name="instEq">
-                            <option>Seleccione una opción</option>
+                        <tr>
+                        <div class="form-group col-md-6">
+                            <label for="name"><strong>Institución:</strong></label>
+                            <select id="institucion" required class="form-control selectpicker" data-size="10" data-live-search="true" data-style="btn-gree" name="instEq">
+                            <option value="-1">Seleccione una opción</option>
                             @foreach ($instituciones as $institucion)
                             <option value="{{$institucion->idInstitucion}}" {{$institucion->idInstitucion==$proyecto[0]->fk_idInstitucion? 'selected="selected"': '' }}> {{ $institucion->nombreInstitucion }}</option>
                             @endforeach
                         </select>
-                        </td>
-                        <td>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="name"><strong>Tipo de invención:</strong></label>
                             <select required id="tipoInvencion" class="form-control selectpicker" data-style="btn-green" name="tipoInv">
                                 <option>Seleccione una opción</option>
                                 @foreach ($invenciones as $invencion)
                                 <option value="{{$invencion->idTipoInvencion}}" {{$invencion->idTipoInvencion==$proyecto[0]->fk_idTipoInvencion? 'selected="selected"': '' }}> {{ $invencion->descripcion }}</option>
                                 @endforeach
                             </select>
-                        </td>
+                        </div>
                     </tr>
                 </tbody>
             </table>
-            <br>
+         
 <h1>2-. Equipo emprendedor/Inventor</h1>
             <table id="datosEmprendedor" class="table table-hover table-condensed" style="width:100%">
                 <thead>
@@ -54,8 +64,9 @@
                 <tbody>
                     @foreach ($participantes as $participante)
                     <tr>
+                     
                         <td><input id="nombrePart" type="text" value="{{ $participante->nombre }}"/></td>
-                        <td>
+                        <td> 
                             <select id="gradoestudios" required class="form-control selectpicker" data-style="btn-green" name="Grado">
                                 <option>Seleccione una opción</option>
                                 @if($participantes == null)
@@ -69,6 +80,7 @@
                                 @endif
                             </select>
                         </td>
+
                         <td>
                             <select id="areaCon" required class="form-control selectpicker" data-style="btn-green" name="area">
                                 @foreach ($areasconocimiento as $areaconocimiento)
@@ -148,27 +160,37 @@
             </table>
 
             <table id="datosAnalisis" class="table table-hover table-condensed" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Usos/Aplicaciones</th>
-                        <th>Vabilidad</th>
-                        <th>Beneficios</th>
-                    </tr>
-                </thead>
+                
                 <tbody>
                     <tr>
-                        <td><textarea class="form-control" id="usoAplicacion">{{ $analisisentorno[0]->usoAplicacion }}</textarea></td>
-                        <td><textarea class="form-control" id="viabilidad">{{ $analisisentorno[0]->viabilidad }}</textarea></td>
-                        <td><textarea class="form-control" id="beneficios">{{ $analisisentorno[0]->beneficios }}</textarea></td>
+                        <div class="form-group col-md-12">
+                        <label for="titulo" ><strong>Usos/Aplicaciones</strong></label>
+                        <textarea style="resize: vertical" class="form-control col-md-6" id="usoAplicacion" rows="6">{{ $analisisentorno[0]->usoAplicacion }}</textarea>
+                    </div>
+                    </tr>
+                    <tr>
+                        <div class="form-group col-md-12">
+                        <label for="titulo" ><strong>Vabilidad</strong></label>
+                        <textarea style="resize: vertical" class="form-control col-md-6" id="viabilidad" rows="6">{{ $analisisentorno[0]->viabilidad }}</textarea>
+                    </div>
+                    </tr>
+                    <tr>
+                        <div class="form-group col-md-12">
+                        <label for="titulo" ><strong>Beneficios</strong></label>
+                        <textarea style="resize: vertical" class="form-control col-md-6" id="beneficios" rows="6">{{ $analisisentorno[0]->beneficios }}</textarea>
+                    </div>
                     </tr>
                 </tbody>
             </table>
 <br>
 <h1>4.- Colaboración con otras IES</h1>
+<div class="form-group col-md-12">
 <textarea style="resize: vertical" class="form-control" rows="6" placeholder="Colaboración con otras IES
 " title="Descripción IES y tipo de colaboración" name="desIES" id="desIES" required="">{{ $colaboracion[0]->descripcion}}</textarea>
+</div>
 <br>
 <h1>5.- Riesgos</h1>
+<div class="form-group col-md-12">
             <table id="datosRiesgos" class="table table-hover table-condensed" style="width:100%">
                 <thead>
                     <tr>
@@ -188,34 +210,44 @@
                             </select>
                             </td>
 
-                        <td><textarea class="form-control" id="descripcionRiesgo">{{ $riesgo->descripcionRiesgo}}</textarea></td>
-                        <td><textarea  class="form-control" id="estrategiaMitigacion">{{ $riesgo->estrategiaMitigacion}}</textarea></td>
+                        <td><textarea style="resize: vertical" class="form-control col-md-6" id="descripcionRiesgo" rows="2">{{ $riesgo->descripcionRiesgo}}</textarea></td>
+                        <td><textarea style="resize: vertical" class="form-control col-md-6" id="estrategiaMitigacion" rows="2">{{ $riesgo->estrategiaMitigacion}}</textarea></td>
                     </tr>
                     @endforeach
                 </tbody>
             </table>
+        </div>
             <br>
 <h1>6.- Análisis del Entorno</h1>
-<textarea style="resize: vertical" class="form-control" rows="6" placeholder="Análisis del entorno
-" title="Descripción IES y tipo de colaboración" name="desIES" required="" id="analisisEntorno">
+<tr>
+<div class="form-group col-md-12">
+<textarea style="resize: vertical" class="form-control col-md-6"  placeholder="Análisis del entorno
+" title="Descripción IES y tipo de colaboración" name="desIES" required="" id="analisisEntorno" rows="6">
     {{ $analisisentorno[0]->descripcionAnalisisEntorno}}
 </textarea>
-
+</div>
+</tr>
 <br>
 <h1>7.- Recursos</h1>
             <table id="datos" class="table table-hover table-condensed" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Humanos</th>
-                        <th>Tecnológicos</th>
-                        <th>Financieros</th>
-                    </tr>
-                </thead>
                 <tbody>
                     <tr>
-                        <td><textarea class="form-control" id="recursosHumanos">{{ $analisisentorno[0]->recursosHumanos}}</textarea></td>
-                        <td><textarea class="form-control" id="recursosTecnologicos">{{ $analisisentorno[0]->recursosTecnologicos}}</textarea></td>
-                        <td><textarea class="form-control" id="recursosFinancieros">{{ $analisisentorno[0]->recursosFinancieros}}</textarea></td>
+                        <div class="form-group col-md-12">
+                        <label for="titulo" ><strong>Humanos</strong></label>
+                        <textarea style="resize: vertical" class="form-control col-md-6" id="recursosHumanos" rows="6">{{ $analisisentorno[0]->recursosHumanos}}</textarea>
+                    </div>
+                    </tr>
+                    <tr>
+                        <div class="form-group col-md-12">
+                        <label for="titulo" ><strong>Tecnológicos</strong></label>
+                        <textarea style="resize: vertical" class="form-control col-md-6" id="recursosTecnologicos" rows="6">{{ $analisisentorno[0]->recursosTecnologicos}}</textarea>
+                    </div>
+                        </tr>
+                        <tr>
+                            <div class="form-group col-md-12">
+                            <label for="titulo" ><strong>Financieros</strong></label>
+                        <textarea style="resize: vertical" class="form-control col-md-6" id="recursosFinancieros" rows="6">{{ $analisisentorno[0]->recursosFinancieros}}</textarea>
+                    </div>
                     </tr>
                 </tbody>
             </table>
