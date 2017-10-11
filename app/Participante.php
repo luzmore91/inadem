f9ad3ae3caa57e4ffae04e33d2576a010569e75d
Mercedes Miranda <mervic1706@gmail.com>
2017-10-10 22:17:21 -0500

    cambios en Participante php meche

--- app/Participante.php
+++ app/Participante.php
@@ -8,6 +8,7 @@ class Participante extends Model
 {
      ////tabla a utilizar
     public $table = 'participante';
+    public $primaryKey = 'idParticipante';
     public $timestamps = false;
 
     public function tecnologia()
