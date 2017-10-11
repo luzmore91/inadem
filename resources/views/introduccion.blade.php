0b7ea71460ad6d1601537fe5284303df8cebc5ee
Luz A. Moreno Rodriguez <luzarelymoreno@gmail.com>
2017-10-11 12:12:25 -0500

    cambio en el texto de introducción Inadem

--- resources/views/introduccion.blade.php
+++ resources/views/introduccion.blade.php
@@ -6,7 +6,8 @@
 <h1>Introducción</h1>
 <p align="justify">
 
-    Descripción de prueba: La Secretaría de Economía, a través del Instituto Nacional del Emprendedor, con fundamento en los numerales 15 y 16 de las Reglas de Operación del Fondo Nacional Emprendedor para el ejercicio fiscal 2017, publicadas en el Diario Oficial de la Federación convoca a Emprendedores, que obtuvieron el diploma del Programa de Incubación en Línea, para que presenten sus solicitudes de apoyo para participar en la Categoría II. Programas de Desarrollo Empresarial, bajo la Convocatoria 2.3 Creación y Fortalecimiento de Empresas Básicas a través del Programa de Incubación en Línea (PIL)
+    El presente registro, tiene como objetivo generar el Banco Nacional de Tecnologías, Proyectos o  Prototipos del Tecnológico Nacional de México (TecNM). Este Banco Nacional, servirá de base para la participación en convocatorias que el TecNM solo o en conjunto con otras instituciones, emita para la participación en fondos, que estimulen el desarrollo tecnológico y la  innovación de alto impacto. Así también busca articular los programas requeridos para desarrollar, validar,  y consolidar las propuestas presentadas e identificar Tecnologías, proyectos o prototipos que se encuentren en posibilidades de ser comercializados o licenciados.
+
 
 </p>
 </div>
